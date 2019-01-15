ed463182d049f96d5b50f48c47266b6d7a5b6a8d
jannahuang <38380050+jannahuang@users.noreply.github.com>
2019-01-15 08:43:06 +0800

    Update monotonic-array.py

--- monotonic-array.py
+++ monotonic-array.py
@@ -13,6 +13,6 @@ class Solution:
         :type A: List[int]
         :rtype: bool
         """
-        return (all(A[i] >= A[i+1] for i in xrange(len(A) - 1)) or 
-                all(A[i] <= A[i+1] for i in xrange(len(A) - 1)))
+        return (all(A[i] >= A[i+1] for i in range(len(A) - 1)) or 
+                all(A[i] <= A[i+1] for i in range(len(A) - 1)))
                 
